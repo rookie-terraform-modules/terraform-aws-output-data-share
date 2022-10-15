98c797c7bda8c18de4b47e4c6fbd7126d3c61320
Oluwatobi Ogunbayo <tobyogunbayo@gmail.com>
2022-10-15 14:11:48 +0200

    fix: remove refactor artifact

--- modules/import_data/s3.tf
+++ modules/import_data/s3.tf
@@ -2,14 +2,14 @@ data "aws_s3_object" "account_accessible_output_export_object" {
   count = var.import_data_config.access_restriction == "all_account_iam_principals" ? 1 : 0
 
   bucket = var.bucket_name
-  key    = "account_accessible_output_exports/${each.value.name}"
+  key    = "account_accessible_output_exports/${var.import_data_config.name}"
 }
 
 data "aws_s3_object" "explicit_iam_groups_accessible_output_export_object" {
   count = var.import_data_config.access_restriction == "explicit_iam_groups" ? 1 : 0
 
   bucket = var.bucket_name
-  key    = "explicit_iam_groups_accessible_output_exports/${each.value.name}"
+  key    = "explicit_iam_groups_accessible_output_exports/${var.import_data_config.name}"
 }
 
 locals {
